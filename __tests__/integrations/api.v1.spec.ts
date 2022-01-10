e52216e1b5c14262ad8155ff8c52ba36833d87ab
tharit.rattananenya <tharit.rt@gmail.com>
2022-01-10 21:19:07 +0700

    [Tech-Task] Add test configuration with Jest

---
+++ __tests__/integrations/api.v1.spec.ts
@@ -0,0 +1,13 @@
+import request from "supertest";
+import server from "../../src/server";
+describe("API V1", () => {
+  it("status check should return empty user", async () => {
+    const status = await request(server)
+      .get("/api/health")
+      .expect("Content-Type", /json/)
+      .then((result) => result.body);
+
+    expect(status.totalClients).toBe(0);
+    expect(status.clients).toEqual([]);
+  });
+});
