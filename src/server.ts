5600ca9d85bf55d05a874186c894a74590248493
tharit.rattananenya <tharit.rt@gmail.com>
2022-01-11 23:11:11 +0700

    [Bug] update static path in dist

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import cors from 'cors'
 import express from 'express'
+import path from 'path'
 import api from './api/v1'
 import { corsOptions } from './middleware/cors'
 
 const server = express()
+const pathToTemplateData = path.join(__dirname, '../public/TemplateData')
+const pathToBuild = path.join(__dirname, '../public/Build')
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(cors(corsOptions))
 
-server.use('/public/TemplateData', express.static(__dirname + '/public/TemplateData'))
-server.use('/public/Build', express.static(__dirname + '/public/Build'))
+server.use('/public/TemplateData', express.static(pathToTemplateData))
+server.use('/public/Build', express.static(pathToBuild))
 server.use(express.static(__dirname + '/public'))
 
 server.use('/api', api)
