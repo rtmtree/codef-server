e794f17f3835086be1cd2cded61dc423f19a0e39
tharit.rattananenya <tharit.rt@gmail.com>
2022-01-24 22:09:51 +0700

    [Update] update local test scripts

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -6,11 +6,13 @@ BASEDIR=$(dirname $0)
 echo 'running test in client'
 
 pushd ${BASEDIR}/../io-client
-sh ../scripts/set-env-vars.sh ../environment/.codef-client.env
+# remove for now (no need to set env for local test)
+# sh ../scripts/set-env-vars.sh ../environment/.codef-client.env
 yarn lint && yarn test
 popd
 
 pushd ${BASEDIR}/../io-server
-sh ../scripts/set-env-vars.sh ../environment/.codef-server.env
+# remove for now (no need to set env for local test)
+# sh ../scripts/set-env-vars.sh ../environment/.codef-server.env
 yarn lint && yarn test
 popd
